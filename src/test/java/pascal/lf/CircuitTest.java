971e0fefb7ed6f834fd8ff09b8d10fba52d26776
JacyCui <jiacaicui@163.com>
2024-07-19 09:30:46 +0800

    test: test interpreter

--- src/test/java/pascal/lf/CircuitTest.java
+++ src/test/java/pascal/lf/CircuitTest.java
@@ -2,11 +2,11 @@ package pascal.lf;
 
 import org.junit.jupiter.api.Test;
 import pascal.lf.frontend.CircuitFrontend;
+import pascal.lf.interpreter.Interpreter;
 import pascal.lf.model.Circuit;
 import pascal.lf.model.Connection;
 import pascal.lf.model.exp.Exp;
 import pascal.lf.model.exp.Exps;
-import pascal.lf.model.exp.Value;
 import pascal.lf.model.exp.Var;
 
 import java.util.Collections;
@@ -31,23 +31,41 @@ public class CircuitTest {
         Var out = Exps.var("out");
         Var counter = Exps.var("counter");
 
-        Value zero = Exps.value(0);
-        Value one = Exps.value(1);
-
-        Exp next = Exps.mux(en, Exps.add(counter, one), counter);
-        Connection c0 = Connection.of(counter, zero);
+        Exp next = Exps.mux(en, Exps.add(counter, Exps.ONE), counter);
+        Connection c0 = Connection.of(counter, Exps.ZERO);
         Connection c1 = Connection.of(counter, next);
         Connection c2 = Connection.of(out, counter);
 
         assertEquals(Set.of(en, out), circuit.getWires());
         assertEquals(Collections.singleton(counter), circuit.getRegs());
-        assertEquals(zero, circuit.getResetValue(counter));
+        assertEquals(Exps.ZERO, circuit.getResetValue(counter));
         assertEquals(Collections.singleton(c0), circuit.getResetConnections());
         assertEquals(Set.of(c1, c2), circuit.getConnections());
         assertEquals(c1, circuit.getDefinerOf(counter));
         assertEquals(c2, circuit.getDefinerOf(out));
         assertEquals(Collections.singleton(c1), circuit.getUsersOf(en));
         assertEquals(Set.of(c1, c2), circuit.getUsersOf(counter));
+
+        Interpreter interpreter = new Interpreter(circuit);
+        interpreter.reset();
+        assertEquals(Exps.ZERO, interpreter.peek(out));
+        interpreter.poke(en, Exps.ZERO);
+        assertEquals(Exps.ZERO, interpreter.peek(out));
+        interpreter.clock();
+        assertEquals(Exps.ZERO, interpreter.peek(out));
+        interpreter.poke(en, Exps.ONE);
+        for (int i = 0; i < 10; i++) {
+            assertEquals(Exps.value(i), interpreter.peek(out));
+            interpreter.clock();
+        }
+        assertEquals(Exps.value(10), interpreter.peek(out));
+        interpreter.poke(en, Exps.ZERO);
+        for (int i = 0; i < 3; i++) {
+            interpreter.clock();
+            assertEquals(Exps.value(10), interpreter.peek(out));
+        }
+        interpreter.reset();
+        assertEquals(Exps.ZERO, interpreter.peek(out));
     }
 
 }
