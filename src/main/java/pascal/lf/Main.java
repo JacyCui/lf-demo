ff86a49135dbd19257ff0f5e0ec7f933ca8fa9eb
JacyCui <jiacaicui@163.com>
2024-07-21 10:05:50 +0800

    feat: add command line interface

--- src/main/java/pascal/lf/Main.java
+++ src/main/java/pascal/lf/Main.java
@@ -1,7 +1,24 @@
 package pascal.lf;
 
+import pascal.lf.frontend.CircuitFrontend;
+import pascal.lf.frontend.ScriptFrontend;
+import pascal.lf.interpreter.Interpreter;
+import pascal.lf.model.Circuit;
+import pascal.lf.model.Script;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: lf <circuit file> <script file>");
+            return;
+        }
+        Circuit circuit = CircuitFrontend.loadFileByName(args[0]).compile();
+        Script script = ScriptFrontend.loadFileByName(args[1]).compile();
+        Interpreter interpreter = new Interpreter(circuit);
+        interpreter.runScript(script);
     }
+
 }
